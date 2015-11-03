f9eae017eee4dced0a849a45aa08aa6a4d1c15c7
danylaksono <thalibul.ilm@gmail.com>
2015-11-03 19:26:44 +0700

    add todo

--- www/js/controllers/dashboardController.js
+++ www/js/controllers/dashboardController.js
@@ -5,13 +5,27 @@ angular.module('starter').controller('DashboardController', ['$scope',
   ) {
 
 
+
     GeolocationService.getLatLong().then(
       function(latLong) {
-        $scope.position = latLong;
+          $scope.position = latLong;
         console.log('LatLong=', $scope.position.lat);
       }
     );
 
-
   }
 ]);
+
+
+/*
+  Todo next:
+    - employ geolocation service into map controller
+    - change tombatossals' angular leaflet with ui-leaflet
+    - add ui bootstrap
+    - add components to dashboard (coordinates (utm and geo), heading, speed, compass)
+    - detect GPS and implement cordova diagnostic plugins
+    - detect network (cordova network information plugin)
+    - add login and implement pouchdb for user auth
+    - add download wizard state
+
+*/
--- www/js/services/geolocationService.js
+++ www/js/services/geolocationService.js
@@ -1,38 +1,47 @@
 angular.module('starter').factory('GeolocationService',
-function($ionicPlatform, $cordovaGeolocation, $q) {
+  function($ionicPlatform, $cordovaGeolocation, $q) {
 
-  var latLong = null;
-  var watchOptions = {
-    timeout: 10000,
-    enableHighAccuracy: true // may cause errors if true
-  };
+    var latLong = null;
+    var watchOptions = {
+      timeout: 10000,
+      enableHighAccuracy: true // may cause errors if true
+    };
 
-  var getLatLong = function(refresh) {
-    var deferred = $q.defer();
-    if (latLong === null || refresh) {
-      console.log('Getting lat long');
-      $cordovaGeolocation.watchPosition(watchOptions).then(
-        null,
-        function(err) {
-          console.log(err);
-          latLong = null
-          deferred.reject('Failed to Get Lat Long')
-        },
-        function(position) {
-          latLong = {
-            'lat': position.coords.latitude,
-            'long': position.coords.longitude
-          }
-          deferred.resolve(latLong);
-        });
-    } else {
-      deferred.resolve(latLong);
+    var getLatLong = function(refresh) {
+      var deferred = $q.defer();
+      if (latLong === null || refresh) {
+        console.log('Getting lat long');
+        var watch = $cordovaGeolocation.watchPosition(watchOptions);
+        watch.then(
+          null,
+          function(err) {
+            console.log(err);
+            latLong = null
+            deferred.reject('Failed to Get Lat Long')
+          },
+          function(position) {
+            latLong = {
+              'lat': position.coords.latitude,
+              'long': position.coords.longitude
+            }
+            deferred.resolve(latLong);
+          });
+      } else {
+        deferred.resolve(latLong);
+      }
+      return deferred.promise;
+    };
+
+    var clearLatLong = function(watch) {
+      console.log('watch location stopped');
+      clear = $cordovaGeolocation.clearWatch(watch);
+      deferred.resolve(clear);
+      return deferred.promise;
     }
-    return deferred.promise;
-  };
 
-  return {
-    getLatLong: getLatLong
-  }
+    return {
+      getLatLong: getLatLong,
+      clearLatLong: clearLatLong
+    }
 
-});
+  });
