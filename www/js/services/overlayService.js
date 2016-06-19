be5104ca6bd46f35440044534784a24ad4ededd4
gameover <bukansatan@gmail.com>
2016-06-19 09:32:25 +0700

    tambah layer kawasan konservasi dan rencana pola ruang laut; ganti input type file di ubah_ava.html dg cordova camera untuk ngambil gambarnya

--- www/js/services/overlayService.js
+++ www/js/services/overlayService.js
@@ -66,6 +66,38 @@ angular.module('starter').factory('OverlayService', ['$http', function($http) {
       layerParams: {
         showOnSelector: true
       }
+    },
+    konservasi: {
+      name: 'Kawasan konservasi Perairan',
+      text: 'Kawasan konservasi Perairan',
+      checked: true,
+      disabled: false,
+      type: 'agsDynamic',
+      url: 'http://103.7.52.65:6080/arcgis/rest/services/16dset_alokasi_ruang_laut/Kawasan_Konservasi_Perairan_/MapServer',
+      visible: true,
+      layerOptions: {
+        crs: L.CRS.EPSG32748,
+        opacity: 1
+      },
+      layerParams: {
+        showOnSelector: true
+      }
+    },
+    polaRuang: {
+      name: 'Rencana Pola Ruang Laut Nasional',
+      text: 'Rencana Pola Ruang Laut Nasional',
+      checked: true,
+      disabled: false,
+      type: 'agsDynamic',
+      url: 'http://103.7.52.65:6080/arcgis/rest/services/16dset_alokasi_ruang_laut/Rencana_Pola_Ruang_Laut_Nasional/MapServer',
+      visible: true,
+      layerOptions: {
+        crs: L.CRS.EPSG32748,
+        opacity: 1
+      },
+      layerParams: {
+        showOnSelector: true
+      }
     }
 
   };
